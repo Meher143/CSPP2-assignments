2089c8259e42ef9461d624bcdce0a57414278eed
Meher143 <krishmeher@msitprogram.net>
2018-08-28 01:04:14 +0530

    original name

--- Assignment-1/Solution.java
+++ Assignment-1/Solution.java
@@ -1,7 +1,7 @@
 public class Solution {
 	public static void main(String[] args) {
-		Str name= Meher;
-		int roll number= 20186107;
-		System.out.println("Name :" +name, "Roll Number :" + roll number);
+		String name= "Meher";
+		int rollnumber= 20186107;
+		System.out.println("Name :" + name + "Roll Number :" + rollnumber);
 	}
 }
