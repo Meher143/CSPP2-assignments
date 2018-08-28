703570366cf4b7af6135afb715fe2ef3dc8015f4
Meher143 <krishmeher@msitprogram.net>
2018-08-28 20:10:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+/** this is a program.**/
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
+final class Solution {
+	/**
+	*Do not modify this main function.
+	**/
+	private Solution() { }
+	/** 
+	* @parem args comment
+	**/
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
@@ -14,11 +19,16 @@ public class Solution {
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
 	*/
-	public static void rootsOfQuadraticEquation(int a, int b, int c){
+	/**
+	* @parem a comment
+	* @parem b comment 
+	* @parem c comment
+	**/
+	public static void rootsOfQuadraticEquation(final int a, final int b, final int c){
 		double r1;
 		double r2;
-		r1 = (-b+Math.sqrt(b*b-4*a*c))/(2*a);
-		r2 = (-b-Math.sqrt(b*b-4*a*c))/(2*a);
-		System.out.println(r1+" "+r2);
+		r1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+		r2 = (-b - Math.sqrt(b * b - 4 * a * c))/(2 * a);
+		System.out.println(r1 + " " + r2);
 	}
 }
