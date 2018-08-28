5795674c487bbed6f0678d25ec7da53053a85d61
Meher143 <krishmeher@msitprogram.net>
2018-08-28 21:45:36 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -1,25 +1,36 @@
 import java.util.Scanner;
+/**
+    *class Solution.
+*/
+public final class Solution {
+    /**
+        *Solution constructor.
+    */
+    private Solution() {
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);      
+    }
+    /**
+    *Do not modify this main function.
+    *@param args arguments.
+    */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    /**
+        *Need to write the power function and print the output.
+        *@param a arguments.
+        *@param b arguments.
+    */
 
-	static long power(final int a, final int b) {
-		if (b ==0) {
-			return 1;
-		}
-		return a * power(a, b - 1);
+    static long power(final int a, final int b) {
+        if (b ==0) {
+            return 1;
+        }
+        return a * power(a, b - 1);
 
-	}
+    }
 }
