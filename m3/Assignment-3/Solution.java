be7653f97f7d63c6c26b396d370f0b4a44385ac0
Meher143 <krishmeher@msitprogram.net>
2018-08-29 19:22:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -1,36 +1,41 @@
 
 import java.util.Scanner;
-/*
-    Do not modify this main function.
-    */
-final class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
     /**
      * Constructs the object.
      */
     private Solution() {
-    }
-    /**
-     * { function_description }.
-     *
-     * @param      args  The arguments
-     */
 
+    }
+/**
+ * greatest common divisor.
+ *
+ * @param      args  The arguments
+ */
     public static void main(final String[] args) {
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
     }
 
-    /*
-    Need to write the gcd function and print the output.
-    */
+    /**
+     * { function_description }.
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
     public static int gcd(final int n1, final int n2) {
         /**
-         * { item_description }
+         * { item_description }.
          */
-        for(int i = Math.min(n1, n2); i >= 1; i--) {
+        for (int i = Math.min(n1, n2); i >= 1; i--) {
             if (n1 % i == 0 && n2 % i == 0) {
                 return i;
             }
