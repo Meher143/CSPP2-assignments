d8b727fe8ef0db57b575d434899603668d511f16
Meher143 <krishmeher@msitprogram.net>
2018-08-29 17:48:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Solution.java
+++ m3/Assignment-2/Solution.java
@@ -17,18 +17,20 @@ public final class Solution {
  */
     public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         int m;
+        final int ten = 10;
+        final int seven = 7;
         for (int i = 1; i <= n; i++) {
             int j = i;
             while (j > 0) {
-                m = j % 10;
-                if (m == 7) {
+                m = j % ten;
+                if (m == seven) {
                     count += 1;
                 }
-                j = j / 10;
+                j = j / ten;
             }
 
         }
