e23753d7267406a580dbe4232d622e65bdb840ed
Meher143 <krishmeher@msitprogram.net>
2018-08-30 19:11:05 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
+    /**
      * Constructs the object.
      */
     private Solution() {
@@ -14,43 +14,43 @@ public final class Solution {
  *
  * @param      args  The arguments
  */
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		int a[][] = new int[m][n];
-		for (int i = 0; i<m; i++) {
-			for (int j = 0; j<n;j++) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		int b[][] = new int[x][y];
-		for (int i = 0;i < x;i++) {
-			for (int j = 0;j < y;j++) {
-				b[i][j] = sc.nextInt();
-			}
-		}
-		if ( m == x && n == y) {
-			int c[][] = new int[m][n];
-			for(int i = 0; i < m;i++){
-				for (int j = 0;j < n;j++){
-					c[i][j] = a[i][j] + b[i][j];
-				}
-			}
-			for (int i = 0; i < m;i++){
-				for (int j = 0;j < n;j++){
-					System.out.print(c[i][j]);
-					if (j != m - 1) {
-						System.out.print(" ");
-					}
-				}
-				System.out.println();
-			}
-		} else {
-			System.out.println("not possible");
-		}			
-	}
+    /* Fill the main function to print resultant of addition of matrices*/
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int a[][] = new int[m][n];
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n;j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int b[][] = new int[x][y];
+        for (int i = 0;i < x;i++) {
+            for (int j = 0;j < y;j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        if ( m == x && n == y) {
+            int c[][] = new int[m][n];
+            for(int i = 0; i < m;i++){
+                for (int j = 0;j < n;j++){
+                    c[i][j] = a[i][j] + b[i][j];
+                }
+            }
+            for (int i = 0; i < m;i++){
+                for (int j = 0;j < n;j++){
+                    System.out.print(c[i][j]);
+                    if (j != m - 1) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("not possible");
+        }           
+    }
 }
