6d3abd99d83edb946b4c8da8d3d2fac7aa89353f
Meher143 <krishmeher@msitprogram.net>
2018-08-30 19:20:14 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -21,27 +21,27 @@ public final class Solution {
         int n = sc.nextInt();
         int a[][] = new int[m][n];
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n;j++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
         int x = sc.nextInt();
         int y = sc.nextInt();
         int b[][] = new int[x][y];
-        for (int i = 0;i < x;i++) {
-            for (int j = 0;j < y;j++) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
                 b[i][j] = sc.nextInt();
             }
         }
         if ( m == x && n == y) {
             int c[][] = new int[m][n];
-            for(int i = 0; i < m;i++) {
-                for (int j = 0;j < n;j++) {
+            for(int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     c[i][j] = a[i][j] + b[i][j];
                 }
             }
             for (int i = 0; i < m;i++) {
-                for (int j = 0;j < n;j++) {
+                for (int j = 0; j < n; j++) {
                     System.out.print(c[i][j]);
                     if (j != m - 1) {
                         System.out.print(" ");
