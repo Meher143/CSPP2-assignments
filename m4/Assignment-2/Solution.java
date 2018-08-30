f5b71f4f11c34df516943761d0f9b9423f68db2d
Meher143 <krishmeher@msitprogram.net>
2018-08-30 19:21:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -40,7 +40,7 @@ public final class Solution {
                     c[i][j] = a[i][j] + b[i][j];
                 }
             }
-            for (int i = 0; i < m;i++) {
+            for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     System.out.print(c[i][j]);
                     if (j != m - 1) {
