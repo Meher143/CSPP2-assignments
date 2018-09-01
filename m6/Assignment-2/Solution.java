621808ffbb444d9429e06ad7d7a7fc23e8d1e818
Meher143 <krishmeher@msitprogram.net>
2018-09-01 11:33:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m6/Assignment-2/Solution.java
+++ m6/Assignment-2/Solution.java
@@ -24,19 +24,19 @@ final class Solution {
      */
     static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
 
-	// write ypur code here
-        for(int i = 0; i<rows; i++) {
+    // write ypur code here
+        for(int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++) {
                 float d = 100;
                 int e = 100;
                 float b = a[i][j]/d;
                 int c = Math.round(b);
-                c = c*e;
+                c = c * e;
                 a[i][j] = c;
             }
-            
+
         }return a;
-	
+    
     }
     /**
      * Main function.
