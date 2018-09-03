a971ef2922cb923dff989c9969cc2783bf7e54f7
Meher143 <krishmeher@msitprogram.net>
2018-09-03 20:28:11 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ m7/Assignment-1/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class InputValidator
+{
+	/*Write the atrributes and methods for InputValidator*/
+	private String data;
+	private int length;
+
+	InputValidator(String name) {
+		data = name;
+		length = data.length();
+	}
+	boolean validateData() {
+		if(length>=6) {
+			return true;
+		}
+		return false;
+	}
+}
+public class Solution
+{
+	public static void main(String args[])
+    {
+    	Scanner s=new Scanner(System.in);
+    	String input=s.next();
+    	InputValidator i=new InputValidator(input);    	
+    	System.out.println(i.validateData());
+
+    }
+
+}
